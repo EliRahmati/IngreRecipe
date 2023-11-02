1879f11db80ce6cf8c15fe4025297cd473bf149e
Elham Rahmati <elhamrahmati1986@gmail.com>
2023-11-02 22:54:29 +0100

    Apply CSS.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AppBar, Toolbar, Typography, Button, Container, Box} from '@mui/material';
 import { Link } from 'react-router-dom';
+import Search from "./Search";
 
 const Home = () => {
   return (
@@ -11,12 +12,15 @@ const Home = () => {
             <Typography variant="h6" component={Link} to="/" sx={{ flexGrow:1, textDecoration: 'none', color: 'white' }}>
               Share and Find Recipes
             </Typography>
-            <Button component={Link} to="/login" color="primary" sx={{ textDecoration: 'none', color: 'white'}}>
-                  Login
-           </Button>
-            <Button component={Link} to="/login" color="primary" sx={{ textDecoration: 'none', color: 'white'}}>
-                  Register
-            </Button>
+                <Button component={Link} to="/my-recipes" color="primary" sx={{ textDecoration: 'none', color: 'white'}}>
+                    My Recipes
+                </Button>
+                <Button component={Link} to="/login" color="primary" sx={{ textDecoration: 'none', color: 'white'}}>
+                      Login
+               </Button>
+                <Button component={Link} to="/login" color="primary" sx={{ textDecoration: 'none', color: 'white'}}>
+                      Register
+                </Button>
           </Toolbar>
         </AppBar>
       </Box>
@@ -32,14 +36,16 @@ const Home = () => {
           />
       </Box>
       <Box>
-
-
-        <Button component={Link} to="/recipes" color="primary" variant="contained">
-                Search
-        </Button>
-        <Button component={Link} to="/my-recipes" color="primary" variant="contained">
-                My Recipes
-        </Button>
+          <Typography variant={'h6'} sx={{ flexGrow:2, textDecoration: 'none', color: 'saddlebrown', fontWeight:600, width: '99%', fontSize: 'h6.fontSize'}}>
+              Welcome to our innovative culinary platform, where the world of cooking is at your fingertips.
+              At IngreRecip, we've simplified the art of meal preparation by offering a user-friendly
+              interface that allows you to input the ingredients you have on hand and instantly receive
+              delectable recipes tailored to your available ingredients. Whether you're an experienced home
+              chef or a novice in the kitchen, our platform is designed to inspire creativity and simplify your
+              cooking journey. Say goodbye to the frustration of not knowing what to cook with the ingredients in
+              your pantry - with IngreRecip, you'll embark on a delightful culinary adventure with every click.
+              Start exploring now and let the magic of cooking come alive in your own kitchen.
+          </Typography>
       </Box>
     </Box>
   );
